9b7015097b00964d3ce864c438547e5c4873f552
haiqiang <haiqiangzou2014@gmail.com>
2014-06-26 14:58:20 -0400

    Update kTip.java

--- kTip Calculator/src/kTip/calculator/kTip.java
+++ kTip Calculator/src/kTip/calculator/kTip.java
@@ -24,7 +24,8 @@ public class kTip extends Activity implements OnClickListener {
 		initialize();
 
 	}
-
+	
+	//initialize all the button and textviews on the interface
 	public void initialize() {
 		totalAmount = (TextView) findViewById(R.id.totalAmount);
 		eachPerson = (TextView) findViewById(R.id.eachPerson);
@@ -36,17 +37,21 @@ public class kTip extends Activity implements OnClickListener {
 		reset.setOnClickListener(this);
 		calculate.setOnClickListener(this);
 	}
-
+	
+	//converts the text into double
 	private void convertEditText() {
 		tiptax = ((Double.parseDouble(tip.getText().toString())) / 100) + 1;
 		bTotal = Double.parseDouble(billTotal.getText().toString());
 		nofPeople = Double.parseDouble(numberofPeople.getText().toString());
 	}
-
+	
+	
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
 
 		switch (arg0.getId()) {
+			
+		//when user click calculate, compute the value for the user and displays it
 		case R.id.calculate:
 			if (billTotal.getText().toString().length() != 0
 					&& numberofPeople.getText().toString().length() != 0
@@ -63,7 +68,8 @@ public class kTip extends Activity implements OnClickListener {
 				y = "";
 			}
 			break;
-
+		
+		//when usser click reset button, it resets all the values on the interface
 		case R.id.reset:
 			totalAmount.setText("0");
 			eachPerson.setText("0");
